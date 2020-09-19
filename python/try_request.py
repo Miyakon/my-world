4d9aa05765d9b444f2d9e3c65a92ab362d6815b6
Miyakon <miyako0901t@gmail.com>
2020-09-20 02:03:30 +0900

    modify try_request.py

--- python/try_request.py
+++ python/try_request.py
@@ -8,7 +8,36 @@ url = 'https://example.com/'
 response = requests.get(url)
 
 print(response)
-# <Response [200]>
+# expected <Response [200]>
 
 print(type(response))
-# <class 'requests.models.Response'>
+# expected <class 'requests.models.Response'
+
+print(response.url)
+# https://example.com/
+
+print (response.status_code)
+# 200
+
+print(response.headers)
+# {'Content-Encoding': 'gzip',
+# 'Accept-Ranges': 'bytes',
+# 'Cache-Control': 'max-age=604800',
+# 'Content-Type': 'text/html',
+# 'Date': 'Thu, 12 Jul 2018 11:58:54 GMT', 
+# 'Etag': '"1541025663"',
+# 'Expires': 'Thu, 19 Jul 2018 11:58:54 GMT',
+# 'Last-Modified': 'Fri, 09 Aug 2013 23:54:35 GMT',
+# 'Server': 'ECS (oxr/8313)',
+# 'Vary': 'Accept-Encoding',
+# 'X-Cache': 'HIT',
+# 'Content-Length': '606'}
+
+print(response.headers['Content-Type'])
+print(response.headers['content-type'])
+
+print(response.encoding)
+
+print('\n\n\n---body---')
+
+print(response.text)
