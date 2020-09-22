b327176a20f32c477e21449d38896cd4460f07fb
Miyakon <miyako0901t@gmail.com>
2020-09-23 01:07:14 +0900

    modify try_request

--- python/try_request.py
+++ python/try_request.py
@@ -41,3 +41,12 @@ print(response.encoding)
 print('\n\n\n---body---')
 
 print(response.text)
+
+url = 'https://www.google.co.jp/search'
+
+params = {'q': '日本代表', 'tbm': 'nws'}
+
+r = requests.get(url, params=params)
+
+print(r.url)
+# https://www.google.co.jp/search?q=%E6%97%A5%E6%9C%AC%E4%BB%A3%E8%A1%A8&tbm=nws
