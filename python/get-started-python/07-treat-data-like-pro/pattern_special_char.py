2a732245c727df0bcc157929b05363b0ab73c13b
Miyakon <Miyako0901t@gmail.com>
2021-01-23 18:41:13 +0900

    modify

--- python/get-started-python/07-treat-data-like-pro/pattern_special_char.py
+++ python/get-started-python/07-treat-data-like-pro/pattern_special_char.py
@@ -8,10 +8,48 @@ if __name__ == "__main__":
     print(printable[0:50])
     print(printable[50:])
 
-    print(re.findall('\d', printable))
-    print(re.findall('\w', printable))
-    print(re.findall('\s', printable))
+    print(re.findall(r'\d', printable))
+    print(re.findall(r'\w', printable))
+    print(re.findall(r'\s', printable))
 
     x = 'abc' + '-/+' + '\u00ea' + '\u0115'
-    print(re.findall('\w', x))
+    print(re.findall(r'\w', x))
 
+    source = '''
+    I wish I may, I wish might
+    Have a dish of fish tonight.
+    '''
+    
+    print('\n\tGroup A')
+    print(source)
+    print(re.findall(r'wish', source))
+    print(re.findall(r'wish|fish', source))
+    print(re.findall(r'^wish', source))
+    print(re.findall(r'^I wish', source))
+    print(re.findall(r'fish$', source))
+    print(re.findall(r'fish tonight.$', source))
+    print(re.findall(r'[wf]ish', source))
+    print(re.findall(r'[wsh]+', source))  # w, s, hのどれかが１つ以上続いている
+    print(re.findall(r'[wsh]*', source))  # 空欄か，w, s, hのどれかが１つ以上続いている
+
+    print('\n\tGroup B')
+    print(source)
+    print(re.findall(r'ght\W', source))
+    print(re.findall(r'I (?=wish)', source))  # Iとスペースの後ろにwishが続く所を探す
+    print(re.findall(r'(?<=I) wish', source))  # スペースとwishの前にIがある所を探す
+    print(re.findall(r'\bfish', source))  # fishの前が単語の境界
+    print(re.findall(r'[^ \s]a', source))  # スペース以外に続くa
+
+    print('\n\tGroup C')
+    print(source)
+    m = re.search(r'(. dish\b).*(\bfish)', source)
+    print(m.group())
+    print(m.groups())
+    print(m.groupdict())
+    m = re.search(r'(?P<DISH>. dish\b).*(?P<FISH>\bfish)', source)
+    print(m.group())
+    print(m.groups())
+    print(m.groupdict())
+    print(m.group('DISH'))
+    print(m.group('FISH'))
+    
