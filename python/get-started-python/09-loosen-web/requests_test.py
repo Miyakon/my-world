c1cbf9c58427778cb417a553dd7a065210795cf4
Miyakon <Miyako0901t@gmail.com>
2021-02-01 07:28:14 +0900

    add bottle3

--- python/get-started-python/09-loosen-web/requests_test.py
+++ python/get-started-python/09-loosen-web/requests_test.py
@@ -6,3 +6,8 @@ if __name__ == "__main__":
     resp = requests.get(url)
     print(resp)
     print(resp.text)
+
+    resp = requests.get('http://localhost:9999/echo/Mothra')
+    if resp.status_code == 200 and \
+        resp.text == 'Say hello to my little friend: Mothra!':
+        print('It worked! That alomost never happens!')
